70f44a0166e98d5814cb4193c5b4823b09fbdbdb
itgsod-martin-haggstrom <martin.haggstrom@elev.it-gymnasiet.se>
2015-03-24 09:46:37 +0100

    ändrade mellanrum

--- dices/sannolikheter1b.rb
+++ dices/sannolikheter1b.rb
@@ -4,9 +4,10 @@ nummer = 0;
   (1..6).each do |dice2|
     nummer += 1;
     if(dice1 + dice2 == 12)
-      puts "##{nummer} #{dice1}, #{dice2}".red
+      print "##{nummer} #{dice1}, #{dice2}".red
     else
-      puts "##{nummer} #{dice1}, #{dice2}".blue
+      print "##{nummer} #{dice1}, #{dice2}".blue
     end
   end
+  puts
 end
--- dices/sannolikheter1d.rb
+++ dices/sannolikheter1d.rb
@@ -2,9 +2,10 @@ require 'colorize'
 (1..6).each do |dice1|
   (1..6).each do |dice2|
     if(dice1 + dice2 > 7)
-      puts "#{dice1 + dice2}".red
+      print "#{dice1 + dice2}".red
     else
-      puts "#{dice1 + dice2}".blue
+      print "#{dice1 + dice2}".blue
     end
   end
+  puts
 end
